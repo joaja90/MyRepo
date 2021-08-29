e103b51673d61c64226a675b76ade46ca59c87e7
joaja90 <yoush218@gmail.com>
2021-08-29 11:34:18 +0200

    testtt

---
+++ 3semester/JS1/js/main.js
@@ -0,0 +1,12 @@
+
+
+let password = "12345";
+
+if(password.length >= 4 && password.length <=7) {
+    console.log("Ja")
+}
+else if { 
+    (password.length> >= 7 && password.length <= 10)
+    console.log("Nej")
+
+}
--- 3semester/main.js
+++
@@ -1,5 +0,0 @@
-let tal = 2;
-let tal2 = 3;
-
-console.log(tal + tal2);
-
